4a474e0f65f34d91803bd7eb6f69adcafdeda21e
Vadim Zendejas <vazendej@microsoft.com>
2018-07-16 12:24:44 +0200

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "ap-northeast-1",
+    BUCKET: "notes-app-2-api-test-attachmentsbucket-1nl2nf1ylqjfm"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "ap-northeast-1",
+    URL: "https://lh9qttb4c9.execute-api.ap-northeast-1.amazonaws.com/test"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "ap-northeast-1",
+    USER_POOL_ID: "ap-northeast-1_gWbHzCFoD",
+    APP_CLIENT_ID: "4tpf6prdfe804rrm76c7k5pr19",
+    IDENTITY_POOL_ID: "ap-northeast-1:55939a24-dc65-4e55-8369-550302b18348"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "ap-northeast-1",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "ap-northeast-1",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "ap-northeast-1",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
